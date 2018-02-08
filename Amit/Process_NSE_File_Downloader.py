6569c2e0a9ed6c7ab623d9be716ac2571156bffb
AmitSapient <34651743+AmitSapient@users.noreply.github.com>
2018-02-08 15:03:17 +0530

    Add files via upload

--- Amit/Process_NSE_File_Downloader.py
+++ Amit/Process_NSE_File_Downloader.py
@@ -21,25 +21,43 @@ class NSE_Result_Calendar_Update_Process:
         self.cur = self.con.cursor()
 
     def csv_reader(self):
+        
         url_prefix = 'https://www.nseindia.com/archives/nsccl/mwpl/nseoi_'
         url_suffix = '.zip'
-        start = '2018-02-01'
-        daterange = pd.date_range(start, periods=5, freq='B')
+        start = '2017-02-01'
+        end = '2018-02-08'
+        daterange = pd.date_range(start, end, freq='B')
         for single_date in daterange:
             date_str = single_date.strftime("%d%m%Y")
             print (date_str)
-            
             url =  url_prefix+date_str+url_suffix
-            print ('url - ', url)
-#        url = urlopen('https://www.nseindia.com/archives/nsccl/mwpl/nseoi_07022018.zip')
-            req = urllib.request.Request(url, headers={'User-Agent' : "Magic Browser"}) 
-            con = urllib.request.urlopen( req )
-            filename = open(date_str+url_suffix, 'wb')
-#        shutil.copyfileobj(con, filename)
-            filename.write(con.read())
+            print ('url - ', url)            
+            try:
+
+    #        url = urlopen('https://www.nseindia.com/archives/nsccl/mwpl/nseoi_07022018.zip')
+                req = urllib.request.Request(url, headers={'User-Agent' : "Magic Browser"}) 
+                con = urllib.request.urlopen( req )
+                filename = 'nseoi_'+date_str+url_suffix
+                file = open(filename, 'wb')
+    #        shutil.copyfileobj(con, filename)
+                file.write(con.read())
+                
+                zf = ZipFile(filename)
+                csvfilename = 'nseoi_'+date_str+'.csv'
+                df = pd.read_csv(zf.open(csvfilename))            
+#                print(df)
+                df.to_csv(csvfilename)
+            except Exception as e:
+                #come here for nationalholiday days
+                print('exception for url - ', url)
+                continue
+
+
         
 
-
+    def extract_zip(self,input_zip):
+        input_zip=ZipFile(input_zip)
+        return {name: input_zip.read(name) for name in input_zip.namelist()}
 
 
 # ----------------------------------------------------------------------
